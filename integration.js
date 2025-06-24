c5f5db4e9bbf0362252f4329645ac255215e0dbd
Asahi <76908462+Asahi-a@users.noreply.github.com>
2025-06-25 03:04:46 +0900

    Update integration.js

--- integration.js
+++ integration.js
@@ -1,9 +1,21 @@
+//モード変更
 let switchsingle = document.getElementById('single');
-switchsingle.addEventListener('click', function() {
-  document.getElementById('css').innerHTML = 'input {width: 100px;}.small {color: #aaa;}.trbd {width: 250px;}.multi{display: none;}.singleswitch{display: none;}';
+switchsingle.addEventListener('click', function () {
+  document.getElementById('css').innerHTML = 'input {width: 100px;}.trbd {width: 250px;}.multi{display: none;}.singleswitch{display: none;}';
 });
 
 let switchmulti = document.getElementById('multi');
-switchmulti.addEventListener('click', function() {
-  document.getElementById('css').innerHTML = 'input{width: 50px;}.small {color: #ddd;}.single{display: none;}.multiswitch{display: none;}';
+switchmulti.addEventListener('click', function () {
+  document.getElementById('css').innerHTML = 'input{width: 50px;}.single{display: none;}.multiswitch{display: none;}';
+});
+
+//CSS変更
+let switchradarsite = document.getElementById('radarsite');
+switchradarsite.addEventListener('click', function () {
+  document.getElementById('RM').setAttribute('href', 'radarsite.css');
+});
+
+let switchmoonlight = document.getElementById('moonlight');
+switchmoonlight.addEventListener('click', function () {
+  document.getElementById('RM').setAttribute('href', 'moonlight.css');
 });
